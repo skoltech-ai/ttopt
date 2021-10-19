130e0ee600fd50c4accf05cff53e5bf7c9079b75
AndreiChertkov <a.chertkov@skoltech.ru>
2021-10-19 18:42:19 +0300

    Add service files and license

---
+++ ttopt/__init__.py
@@ -0,0 +1,8 @@
+__version__ = '0.1'
+
+
+from .ttopt import TTOpt
+from .ttopt_raw import ttopt
+from .ttopt_raw import ttopt_find
+from .ttopt_raw import ttopt_fs
+from .ttopt_raw import ttopt_init
